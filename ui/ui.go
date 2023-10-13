a12eda872efd43d74a8a3f20766fdc3711cfd82d
Dave Marsh <davemarsh77@gmail.com>
2023-10-13 12:21:17 -0400

    remove redundant list

--- ui/ui.go
+++ ui/ui.go
@@ -9,8 +9,6 @@ import (
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/layout"
-	"fyne.io/fyne/v2/theme"
-	"fyne.io/fyne/v2/widget"
 )
 
 type Ui struct {
@@ -19,17 +17,17 @@ type Ui struct {
 	model         *data.Model
 	mainContainer *fyne.Container
 
-	title       *widget.Label
+	// title       *widget.Label
 	strip       *LightStrip
 	stripPlayer *LightStripPlayer
-	layerList   *LayerList
+	// layerList   *LayerList
 
 	playContainer *fyne.Container
 	frameView     *fyne.Container
-	layerView     *fyne.Container
+	// layerView     *fyne.Container
 
-	effectsIcon *widget.Icon
-	frameIcon   *widget.Icon
+	// effectsIcon *widget.Icon
+	// frameIcon   *widget.Icon
 }
 
 func NewUi(app fyne.App, window fyne.Window, model *data.Model) *Ui {
@@ -51,37 +49,19 @@ func (ui *Ui) BuildContent() *fyne.Container {
 		fmt.Println(err)
 	}
 
-	ui.effectsIcon = res.NewAppIcon(res.EffectsIcon)
-	ui.frameIcon = res.NewAppIcon(res.FrameIcon)
-
-	ui.title = widget.NewLabelWithData(ui.model.Title)
-	ui.title.TextStyle = fyne.TextStyle{Bold: true, Italic: true}
-	titleBox := container.New(layout.NewCenterLayout(),
-		container.NewHBox(ui.effectsIcon, ui.title))
-
 	ui.strip = NewLightStrip(res.StripLength, res.StripRows, res.StripInterval)
 	ui.stripPlayer = NewLightStripPlayer(ui.strip, ui.model.Frame)
 
 	stripTools := container.New(layout.NewCenterLayout(), ui.stripPlayer)
-	ui.playContainer = container.NewVBox(titleBox, ui.strip, stripTools)
+	// ui.playContainer = container.NewVBox(titleBox, ui.strip, stripTools)
+	ui.playContainer = container.NewBorder(nil, stripTools, nil, nil, ui.strip)
 
-	toLayerButton := widget.NewButtonWithIcon("", theme.GridIcon(),
-		ui.changeViewLayer)
-	frameSelector := container.NewBorder(nil, nil, toLayerButton, nil,
-		NewFrameSelect(ui.model))
-	ui.layerList = NewLayerList(ui.model, ui.changeViewLayer)
-	ui.frameView = container.NewBorder(frameSelector, nil, nil, nil,
-		ui.layerList.List)
-
-	toFrameButton := widget.NewButtonWithIcon("", theme.ListIcon(),
-		ui.changeViewFrame)
-	layerSelector := container.NewBorder(nil, nil, toFrameButton, nil,
-		NewLayerSelect(ui.model))
+	selectors := container.NewVBox(NewFrameSelect(ui.model), NewLayerSelect(ui.model))
 	form := NewLayerForm(ui.model, ui.changeViewFrame)
-	ui.layerView = container.NewBorder(layerSelector, nil, nil, nil,
+	ui.frameView = container.NewBorder(selectors, nil, nil, nil,
 		form.AppTabs)
 
-	ui.mainContainer = container.NewBorder(ui.playContainer, nil, nil, nil, ui.frameView)
+	ui.mainContainer = container.NewBorder(nil, ui.frameView, nil, nil, ui.playContainer)
 	ui.AddListeners()
 	return ui.mainContainer
 }
@@ -111,13 +91,13 @@ func (ui *Ui) changeViewFrame() {
 	ui.setView(ui.frameView)
 }
 
-func (ui *Ui) changeViewLayer() {
-	ui.setView(ui.layerView)
-}
+// func (ui *Ui) changeViewLayer() {
+// 	ui.setView(ui.layerView)
+// }
 
-func (ui *Ui) SetTitle(title string) {
-	ui.title.SetText(title)
-}
+// func (ui *Ui) SetTitle(title string) {
+// 	ui.title.SetText(title)
+// }
 
 func (ui *Ui) SetWindowTitle(title string) {
 	ui.window.SetTitle(title)
